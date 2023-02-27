f9bf65cc3ca6c65d3590e6b9d5352c3db0f6c9b3
Treyen Wilson <126518692+TreyenSSCC@users.noreply.github.com>
2023-02-27 21:18:52 +0000

    Version 0.0.2

--- bucket.py
+++ bucket.py
@@ -1,5 +1,3 @@
-# Version 0.0.1 - 2/27/2023 4:12 PM
+# Version 0.0.2 - 2/27/2023 4:18 PM
 
-syntax = ["AND", "OR", "IF", "ELSE", "PRINT", "VAR"]
-
-print(syntax[0])
+syntax = ["AND", "OR", "IF", "ELSE", "PRINT", "VAR", "WHILE"]
