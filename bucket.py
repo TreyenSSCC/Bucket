486f97bd4e135a5665ed31e53efd1185a35a46b2
Treyen Wilson <126518692+TreyenSSCC@users.noreply.github.com>
2023-02-27 16:28:50 -0500

    Update bucket.py

--- bucket.py
+++ bucket.py
@@ -1,3 +1,4 @@
-# Version 0.0.2 - 2/27/2023 4:18 PM
+# Version 0.0.3 - 2/27/2023 4:25 PM
 
 syntax = ["AND", "OR", "IF", "ELSE", "PRINT", "VAR", "WHILE"]
+# The syntax list will contain all of the syntax for Bucket.
