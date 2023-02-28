99c7ece4fe35d523c5e41b1da63ffdb191474cba
Treyen Wilson <126518692+TreyenSSCC@users.noreply.github.com>
2023-02-28 22:48:34 +0000

    Version 0.0.4

--- bucket.py
+++ bucket.py
@@ -1,4 +1,14 @@
-# Version 0.0.3 - 2/27/2023 4:29 PM
+# Version 0.0.4 - 2/28/2023 5:48 PM
 
 syntax = ["AND", "OR", "IF", "ELSE", "PRINT", "VAR", "WHILE"]
 # The syntax list will contain all of the syntax for Bucket.
+
+
+def Bucket():# This is the main function of the program. This is where Bucket will perform most of it's tasks.
+    code_line = 0
+    while True:
+        code_line+=1
+        print(code_line, ": ", sep="", end="")
+        user_code = input("")
+
+Bucket()
