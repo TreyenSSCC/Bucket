ad00a4f0df4362cf84b823b8b73ab0a690af8f28
Treyen Wilson <126518692+TreyenSSCC@users.noreply.github.com>
2023-02-28 23:15:43 +0000

    Version 1.0.3 Version 0.0.5

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ This is a programming language that I am creating.
 I am creating Bucket as the final project for my Computer Information Tech Capstone course at Southern State Community College.
 
 Bucket may or may not be continued after the completion of the final project.
+
+
+Update Log
+
+Version 0.0.5: Made syntax all lowercase. Added an exit and quit function that allows users to exit the program. User input is turned lowercase when grabbed now.
--- bucket.py
+++ bucket.py
@@ -1,6 +1,6 @@
-# Version 0.0.4 - 2/28/2023 5:48 PM
+# Version 0.0.5 - 2/28/2023 6:13 PM
 
-syntax = ["AND", "OR", "IF", "ELSE", "PRINT", "VAR", "WHILE"]
+syntax = ["and", "or", "if", "else", "print", "var", "while"]
 # The syntax list will contain all of the syntax for Bucket.
 
 
@@ -9,6 +9,8 @@ def Bucket():# This is the main function of the program. This is where Bucket wi
     while True:
         code_line+=1
         print(code_line, ": ", sep="", end="")
-        user_code = input("")
+        user_code = input("").lower()
+        if (user_code==("quit") or user_code=="exit"):
+            break
 
 Bucket()
