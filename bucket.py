41d67970882626c529589aa0cef58ce5ae87d20d
Treyen Wilson <126518692+TreyenSSCC@users.noreply.github.com>
2023-03-14 17:00:26 -0400

    Version 0.0.7

--- bucket.py
+++ bucket.py
@@ -1,6 +1,6 @@
-# Version 0.0.6 - 3/13/2023 4:45 PM
+# Version 0.0.7 - 3/14/2023 5:00 PM
 
-syntax = ["and", "or", "if", "else", "print", "var", "while", "end of list"]
+syntax = ["and", "or", "if", "else", "print", "var", "while", "func", "end of list"]
 # The syntax list will contain all of the syntax for Bucket.
 
 
@@ -13,7 +13,7 @@ def Bucket():# This is the main function of the program. This is where Bucket wi
         user_code = input("").lower()
         if (user_code==("quit") or user_code=="exit"):
             break
-        while(x<7): # Detects if the user entered any syntax.
+        while(x<8): # Detects if the user entered any syntax. #Use len() command to grab the length of the list like x<len(syntax)-1
             if(user_code==syntax[x]):
                 print(syntax[x])
             x+=1
